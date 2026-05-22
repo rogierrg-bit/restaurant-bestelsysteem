bcea7ec5f0c019270797d65f9408ea83a416b495
Rogier Konietzka <rogierkonietzka@gmail.com>
2026-05-22 16:27:05 +0200

    Initial project setup: VlammendVarken solution met Gerecht-class

---
+++ VlammendVarken/Gerecht.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VlammendVarken
+{
+    public class Gerecht
+    {
+        public string Naam { get; set; }
+        public decimal Prijs { get; set; }
+
+        public Gerecht(string naam, decimal prijs)
+        {
+            Naam = naam;
+            Prijs = prijs;
+        }
+
+        public void Bestel()
+        {
+            Console.WriteLine($"Bestelling geplaatst: {Naam} - €{Prijs}");
+        }
+    }
+}
---
+++ VlammendVarken/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace VlammendVarken
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Gerecht pizza = new Gerecht("Pizza", 7.99m);
+            pizza.Bestel();
+
+            Gerecht hamburger = new Gerecht("Hamburger", 6.49m);
+            hamburger.Bestel();
+        }
+    }
+}
